f16b449317351c5a08d9c0ffaec0af963f795ca1
zhangqwei <quanwei.zhang@gmail.com>
2022-02-15 16:38:58 +0800

    Pointer to define, use, and tell nil or not

--- basic/point.go
+++ basic/point.go
@@ -23,4 +23,29 @@ func main() {
 	//为指针变量赋值。
 	//访问指针变量中指向地址的值。
 
+	// 声明指针变量
+	var ptr *int
+
+	//指针变量 存储的地址
+	ptr = &currentSecond
+
+	fmt.Printf("the currentSecond address is: %x \n", &currentSecond)
+
+	//指针变量 ptr 存储的地址
+	fmt.Printf("the address contained in ptr is: %x \n", ptr)
+
+	//使用指针 ptr 访问值
+	fmt.Printf("the value using ptr access is: %d \n", *ptr)
+
+	//当一个指针被定义后没有分配到任何变量时，它的值为 nil
+	var ptrnil *int
+	fmt.Printf("the ptrnil value is: %x \n", ptrnil)
+
+	//空指针判断  yes   no
+	//ptr 不是空指针  if(ptr != nil)
+	//ptr 是空指针if(ptr == nil)
+
+	if ptrnil == nil {
+		fmt.Printf("the ptrnil value is: nil \n")
+	}
 }
